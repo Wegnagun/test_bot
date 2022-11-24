f54123d91aab06cfd5605cc9f746c96054d274ee
Александр Фокин <wegnagun@bk.ru>
2022-11-24 17:31:03 +0300

    some changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont
 
 load_dotenv()
 
@@ -25,14 +26,32 @@ def check_tokens() -> bool:
 
 
 async def save_user_foto(user_id):
-    filename = datetime.now().strftime(f"%m-%d-%Y_%H-%M")
+    filename = datetime.now().strftime(f"%m-%d-%Y_%H-%M")   #  верни бля двоеточие во времени!!!!
     user_profile_photo = await dp.bot.get_user_profile_photos(user_id)
-    photo_path = f"./media/{filename}_{user_id}.jpg"
+    file_name = f"./media/{filename}_{user_id}.jpg"
     os.makedirs(os.path.dirname('./media/'), exist_ok=True)
     if user_profile_photo.total_count != 0:
         await user_profile_photo.photos[0][-1].download(
-            destination_file=photo_path
+            destination_file=file_name
         )
+    await add_text_to_image(file_name, user_id)
+
+
+async def add_text_to_image(file_name, user_id):
+    default_image = Image.open(file_name)
+    font = ImageFont.truetype('Lobster-Regular.ttf', size=45)
+    final_image = ImageDraw.Draw(default_image)
+    final_image.text(
+        (60, 550),
+        'Фоточка, а ней красоточка!',
+        fill='#fffcfe',
+        font=font,
+        stroke_width=2,
+        stroke_fill='#030203'
+    )
+    default_image.save(file_name)
+    photo = open(file_name, 'rb')
+    await bot.send_photo(chat_id=user_id, photo=photo)
 
 
 @dp.message_handler(lambda message: message.text not in BOTS_COMMAND)
@@ -52,11 +71,12 @@ async def give_help(message: types.Message):
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     """Функция реакции на команду /start."""
-    await save_user_foto(message.from_user.id)
+    user_id = message.from_user.id
+    await save_user_foto(user_id)
     me = await bot.get_me()
     await message.reply(f'\U0001F916 Привет, {message.from_user.full_name}, '
-                        f'меня зовут {me.full_name}, '
-                        f'Напиши мне название города и я пришлю погоду')
+                        f'меня зовут {me.full_name}!\n'
+                        f'Я делаю подписи к фото =)')
 
 
 if __name__ == '__main__':
